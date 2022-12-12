b1135fbc6c71a22ecab4c0b9f049671948fc6acc
nmollers <210981@vistacollege.nl>
2022-12-12 11:30:37 +0100

    ..

---
+++ test.py
@@ -0,0 +1,15 @@
+class Character():
+    name = ''
+    health = 1.0
+    mana = 1.0
+    attack = (1, 2)
+    defense = 1
+    magic = 1
+    res_lightning = 0
+    res_fire = 0
+    res_magic = 0
+
+    def __init__(self, **kwargs):
+        self.name
+    
+
