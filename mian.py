c853611f9eb0123ab08d50d26fc1cf445ebde231
JasonLukassen <113337677+JasonLukassen@users.noreply.github.com>
2022-12-12 14:55:45 +0100

    update
    stats dictionary added

--- mian.py
+++ mian.py
@@ -6,20 +6,25 @@ import time
 import random
 from tkinter import *
 
+stats = {"kracht":None,
+         "HP":None,
+         "XP":None}
+
+
 
 inventory = {"potions":0,
              "sword": None,
              "shield": None,
              "magic wand":None}
              
-print(inventory)
+# print(inventory)
 
 chest = 0
 chestFound = 2
 
 inventory["potions"] += chestFound
 
-print(inventory)
+# print(inventory)
 
 
 
