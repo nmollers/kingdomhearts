46bb9d3899e7055e2eb952660b3b60a69894216c
JasonLukassen <113337677+JasonLukassen@users.noreply.github.com>
2022-12-15 14:00:57 +0100

    update

---
+++ fights.py
@@ -0,0 +1,39 @@
+# fight 1
+print("moves : attack")
+move = input("Enter what you will do: ")
+if move == "attack":
+    print("you damaged the Shadow")
+    print("Choose your following move:")
+    print("moves : attack")
+    move2 = input("Enter what you will do; ")
+    if move2 == "attack":
+        print("You defeated the shadow.")
+        #   continues with the story
+        
+else:
+    print("invalid choice.")
+
+#-------------------------------------------------------------------------------------
+# fight 2
+print("moves : attack / block")
+move = input("Enter what you will do: ")
+if move == "attack":
+    print("you damaged the Shadow")
+    print("Choose your following move:")
+    print("moves : attack block")
+    move2 = input("Enter what you will do: ")
+    if move2 == "attack":
+        print("you damaged the Shadow")
+        print("Choose your following move:")
+        print("moves : attack")
+        move3 = input("Enter what you will do: ")
+        if move3 == "attack":
+            print("You defeated the shadow.")
+        #   continues with the story
+if move == "block":
+    
+        
+else:
+    print("invalid choice.")
+#-------------------------------------------------------------------------------------
+# fight 3
